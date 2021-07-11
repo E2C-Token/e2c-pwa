a9e06f7e58784188760545958485b7e1e7a5a1b4
Athus Oliveira <atsaudio@gmail.com>
2021-07-11 00:03:31 -0300

    incluido emissão de tokens no painel

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -3,12 +3,18 @@
     <div>      
       <WaitingToLiquidate />
     </div>
+    <br />
+    <br />
+    <div>      
+      <EmmitTokens />
+    </div>
   </div>
 </template>
 <script>
 import WaitingToLiquidate from "../components/WaitingToLiquidate"
+import EmmitTokens from "../components/EmmitTokens"
 export default {
   name: "Transactions",
-  components: { WaitingToLiquidate },
+  components: { WaitingToLiquidate, EmmitTokens }
 };
 </script>
