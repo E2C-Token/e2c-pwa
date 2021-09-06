b58c2c04ce25378400813749901d8f7b9e3bd370
Athus Oliveira <atsaudio@gmail.com>
2021-09-05 23:28:55 -0300

    Ajuste de layout

--- src/components/AvaiableList.vue
+++ src/components/AvaiableList.vue
@@ -22,7 +22,7 @@
       aria-labelledby="avaiableDetailsLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="avaiableDetailsLabel">
