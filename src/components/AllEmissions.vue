169fb7cc75b7ea2425cc2058cc796919f401aacf
Athus Oliveira <atsaudio@gmail.com>
2022-02-06 05:03:40 -0300

    Adicionado ChartJs-Sankey

--- src/components/AllEmissions.vue
+++ src/components/AllEmissions.vue
@@ -20,16 +20,54 @@
           <td>{{ i.initialAmount }}</td>
         </tr>
       </tbody>
-    </table>    
+    </table>
+    <div class="chart">
+      <canvas id="chart"></canvas>
+    </div>   
   </div>
 </template>
 <script>
+import { Chart, LinearScale } from "chart.js";
+import { SankeyController, Flow } from "chartjs-chart-sankey";
+
+Chart.register(LinearScale, SankeyController, Flow);
+
 export default {
   name: "AllEmissions",
+  mounted: function() {
+    var ctx = document.getElementById("chart").getContext("2d");
+
+    var colors = {
+      Emissions: "blue",
+      Liquidations: "orange",  
+    };
+
+    function getColor(name) {
+      return colors[name] || "green";
+    }
+
+    new Chart(ctx, {
+      type: "sankey",
+      data: {
+        datasets: [
+          {
+            data: [
+              { from: 'Emissions', to: 'Liquidations', flow: 15 }  
+            ],
+            colorFrom: c => getColor(c.dataset.data[c.dataIndex].from),
+            colorTo: c => getColor(c.dataset.data[c.dataIndex].to)
+          }
+        ]
+      }
+    });
+  },
   computed: {
     allEmissions() {
       return this.$store.state.tokens;
     },
+    allLiquidations() {
+      return this.$store.state.liquidations;
+    }
   },
 };
 </script>
